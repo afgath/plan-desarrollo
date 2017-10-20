f7fc7bcd0538fbf3c4457c28527450a2af41a506
Brahian Calderón Calvo <brahian97@outlook.com>
2017-10-19 21:07:58 -0500

    Pantalla registro
    Pantalla registro (sin managedBean)

---
+++ src/main/java/com/vortexbird/gluon/plan/presentation/backingBeans/RegistroVista.java
@@ -0,0 +1,10 @@
+package com.vortexbird.gluon.plan.presentation.backingBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class RegistroVista {
+
+}
